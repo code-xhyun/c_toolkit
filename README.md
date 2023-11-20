7b6afefd28fbb9789152b0259f84c008634efb11
code-xhyun <shhong@taling.me>
2023-11-20 14:27:33 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # c_toolkit
 corbin's cli tooklit
+
+
+
+## Usage
+
+```bash
+# install
+ npm install
+ npm run build
+
+ npm install -g .
+```
+
+```bash
+# run
+ctoolkit help <command>
+ctoolkit kill --port <port>
+```
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import { Command } from "commander";
+import { exec } from "child_process";
+
+function killProcessOnPort(port: number | string) {
+  exec(
+    `lsof -i :${port} | grep LISTEN | awk '{print $2}'`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(`exec error: ${err}`);
+        return;
+      }
+
+      const processId = stdout.trim();
+      if (processId) {
+        exec(`kill -9 ${processId}`, (err, stdout, stderr) => {
+          if (err) {
+            console.error(`exec error: ${err}`);
+            return;
+          }
+          console.log(`Process on port ${port} has been killed.`);
+        });
+      } else {
+        console.log(`No process listening on port ${port}`);
+      }
+    }
+  );
+}
+
+const program = new Command();
+
+program
+  .command("kill")
+  .description("Kill process on port")
+  .option("--port <port>", "Port to kill")
+  .action((options) => {
+    if (options.port) {
+      killProcessOnPort(options.port);
+    }
+  });
+
+program.parse();
